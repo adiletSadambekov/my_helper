5cf1c1b98bd11aa879225dbde5f4e4b6264f529b
Адилет Садамбеков <adiletsadambekov@MacBook-Pro-Adilet.local>
2022-04-18 22:30:34 +0300

    Added function send message in time

--- func_db.py
+++ func_db.py
@@ -24,7 +24,7 @@ class DatabaseInterface:
         finally:
             self.conn.close()
 
-    def add_user(self, id_user, username, userlogin, is_active):
+    def add_user(self, id_user, username, userlogin, is_active, chat_id):
         try:
             check_user = self.cur.execute(f"select count(id_user) from user where id_user={id_user};")
             n = check_user.fetchone()
@@ -32,7 +32,7 @@ class DatabaseInterface:
                 return True, text_repeat_start
             else:
                 d = int(time.time())
-                self.cur.execute("INSERT INTO user (id_user, username, userlogin, sub_time, is_active) VALUES (?, ?, ?, ?, ?)", (id_user, username, userlogin, d, is_active))
+                self.cur.execute("INSERT INTO user (id_user, username, userlogin, sub_time, is_active, chat_id) VALUES (?, ?, ?, ?, ?, ?)", (id_user, username, userlogin, d, is_active, chat_id))
                 self.conn.commit()
                 return True, text_new_user_successfully
         except sqlite3.IntegrityError:
@@ -46,11 +46,22 @@ class DatabaseInterface:
 
     def get_all_id_user(self):
         try:
-            exe = self.conn.execute('select id_user from user;')
+            exe = self.cur.execute('select id_user from user;')
             id_users = exe.fetchall()
             return id_users, True
-        #except:
-        #    print('happened exeption')
-        #    return False
+        except Exception as e:
+            print(f"Happened following except {e}")
+            return False
+        finally:
+            self.conn.close()
+    
+    def get_chat_id(self):
+        try:
+            exe = self.cur.execute('select chat_id from user;')
+            chat_id = exe.fetchall()
+            return chat_id
+        except Exception as e:
+            (f'Happened following except {e}')
+            return False
         finally:
             self.conn.close()
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,4 +1,5 @@
-from config import API_TOKEN
+from time import time
+from config import API_TOKEN, comands
 from parser import PageParse
 from func_db import DatabaseInterface
 
@@ -14,20 +15,26 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 
+
+
+
 async def  mailings(): #send a message to users
     while True:
         times = PageParse().get_items_times()
-        users = DatabaseInterface().get_all_id_user()
-        for id in users:
-            await bot.send_message(id, '\n'.join(times))
-        asyncio.sleep(30)
+        chat_id = DatabaseInterface().get_chat_id()
+        if chat_id:
+            for chat_id in chat_id[0]:
+                asyncio.sleep(3)
+                await bot.send_message(chat_id, '\n'.join(times))
+                asyncio.sleep(3)
+        await asyncio.sleep(30)
 
 
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=['start'])
 async def start_bot(message: types.Message):
     form = message.from_user
-    add = DatabaseInterface().add_user(form.id, form.username, form.first_name, 1)
+    add = DatabaseInterface().add_user(form.id, form.username, form.first_name, 1, message.chat.id)
     if add:
         await message.reply(add[1])
     else:
@@ -39,6 +46,13 @@ async def start_bot(message: types.Message):
             await bot.send_message(id, '\n'.join(times))
         asyncio.sleep(30)
 
+
+@dp.message_handler(commands=['help'])
+async def helper(message: types.Message):
+    await message.reply(f"I can fulfill folowing comands: {comands}")
+
+
+
 @dp.message_handler(commands='time')
 async def get_times(message: types.Message):
     times = PageParse().get_items_times()
@@ -49,4 +63,5 @@ async def cmd_test1(message: types.Message):
     await message.reply(f"Hello {message.from_user.first_name}")
 
 if __name__ == '__main__':
+    asyncio.run(mailings())
     executor.start_polling(dp, skip_updates=True)
