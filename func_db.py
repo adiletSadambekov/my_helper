733344d23dcfaefed92bd7527340ec81f2e91c13
Адилет Садамбеков <adiletsadambekov@MacBook-Pro-Adilet.local>
2022-04-21 22:45:53 +0300

    first working version

--- func_db.py
+++ func_db.py
@@ -1,6 +1,22 @@
-from config import text_new_user_successfully, text_new_user_invalid, text_repeat_start
+#importing library orm system sqlalchemy
+from sqlalchemy.orm import sessionmaker
+
+#importing models
+from models import Users
+
+from config import text_new_user_successfully, text_new_user_invalid, text_repeat_start, engin
 import time
 import sqlite3
+
+
+def add_user():
+    new_user = Users(id_user=343, username='@adi', userlogin='adi_kg')
+    Dbsession = sessionmaker(bind=engin)
+    s = Dbsession()
+    s.add(new_user)
+    s.commit()
+
+
 
 
 class DatabaseInterface:
@@ -24,25 +40,43 @@ class DatabaseInterface:
         finally:
             self.conn.close()
 
-    def add_user(self, id_user, username, userlogin, is_active, chat_id):
+    def add_user(self, id_user, username, userlogin, chat_id):
         try:
             check_user = self.cur.execute(f"select count(id_user) from user where id_user={id_user};")
             n = check_user.fetchone()
             if int(n[0]) > 0:
-                return True, text_repeat_start
+                self.cur.execute(f"update user set is_active=1 where id_user={id_user}")
+                self.conn.commit()
+                return True, 'Вы обнавили подписку'
             else:
                 d = int(time.time())
-                self.cur.execute("INSERT INTO user (id_user, username, userlogin, sub_time, is_active, chat_id) VALUES (?, ?, ?, ?, ?, ?)", (id_user, username, userlogin, d, is_active, chat_id))
+                self.cur.execute("INSERT INTO user (id_user, username, userlogin, sub_time, is_active, chat_id) VALUES (?, ?, ?, ?, 1, ?)", (id_user, username, userlogin, d, chat_id))
                 self.conn.commit()
                 return True, text_new_user_successfully
         except sqlite3.IntegrityError:
             print('Invalid data type') #add logfile and write in that messagen about it string
             return False
-        except:
-            print('an unknown error has occurred')
-            return False, text_new_user_invalid
+        except Exception as e:
+            return False, str(e)
         finally:
             self.conn.close()
+    
+    #funcion for unsubcribtion
+    def not_active(self, id_user):
+        try:
+            exe = self.cur.execute(f"select count(id_user) from user where id_user = {id_user}").fetchone()
+            if int(exe[0]) == 0:
+                return True, 'You not subscribe'
+            else:
+                self.cur.execute(f"update user set is_active=0 where id_user = {id_user}")
+                self.conn.commit()
+                return True, 'You succefully unsubscribe'
+        except Exception as e:
+            print(f"Happened following exeptions {e}")
+            return False
+        finally:
+            self.conn.close()
+
 
     def get_all_id_user(self):
         try:
@@ -57,7 +91,7 @@ class DatabaseInterface:
     
     def get_chat_id(self):
         try:
-            exe = self.cur.execute('select chat_id from user;')
+            exe = self.cur.execute('select chat_id from user where is_active=1;')
             chat_id = exe.fetchall()
             return chat_id
         except Exception as e:
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,4 +1,3 @@
-from time import time
 from config import API_TOKEN, comands
 from parser import PageParse
 from func_db import DatabaseInterface
@@ -17,32 +16,34 @@ dp = Dispatcher(bot)
 
 
 
-
-async def  mailings(): #send a message to users
+async def  mailings(for_sleep): #send a message to users
     while True:
         times = PageParse().get_items_times()
         chat_id = DatabaseInterface().get_chat_id()
         if chat_id:
-            for chat_id in chat_id[0]:
-                await bot.send_message(chat_id, '\n'.join(times))
-        await asyncio.sleep(30)
+            for chat_id in chat_id:
+                await bot.send_message(chat_id[0], '\n\n'.join(times))
+        await asyncio.sleep(for_sleep)
 
 
 
 @dp.message_handler(commands=['start'])
 async def start_bot(message: types.Message):
     form = message.from_user
-    add = DatabaseInterface().add_user(form.id, form.username, form.first_name, 1, message.chat.id)
+    add = DatabaseInterface().add_user(form.id, form.username, form.first_name, message.chat.id)
     if add:
         await message.reply(add[1])
     else:
         await message.reply(add[1])
-    while True:
-        times = PageParse().get_items_times()
-        users = DatabaseInterface().get_all_id_user()
-        for id in users:
-            await bot.send_message(id, '\n'.join(times))
-        asyncio.sleep(30)
+
+
+@dp.message_handler(commands=['unsubscribe'])
+async def unsub(message: types.Message):
+    uns = DatabaseInterface().not_active(message.from_user.id)
+    if uns:
+        await message.reply(uns[1])
+    else:
+        await message.reply('Happened error')
 
 
 @dp.message_handler(commands=['help'])
@@ -60,6 +61,8 @@ async def get_times(message: types.Message):
 async def cmd_test1(message: types.Message):
     await message.reply(f"Hello {message.from_user.first_name}")
 
+
 if __name__ == '__main__':
-    asyncio.run(mailings())
+    loop = asyncio.get_event_loop()
+    loop.create_task(mailings(30))
     executor.start_polling(dp, skip_updates=True)
