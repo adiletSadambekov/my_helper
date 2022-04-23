d51f5931d2ad1f88b22c3bc5b9ba47c1d004ab98
Адилет Садамбеков <adiletsadambekov@MacBook-Pro-Adilet.local>
2022-04-23 17:04:57 +0300

    fixed mistake git

---
+++ models.py
@@ -0,0 +1,21 @@
+from array import array
+from email.policy import default
+from config import engin
+
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, Boolean
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Users(Base):
+    __tablename__ = 'subs'
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, unique=True)
+    username = Column(String)
+    userlogin = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    def __repr__(self):
+        return "<User: (%s, %s, %s, %s)>" % (self.id_user, self.username, self.userlogin, self.is_active)
+    
